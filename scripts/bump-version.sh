56f23e83a606e192e0db4a409d03b6f0e12da743
Coleby Kent <coleby210@gmail.com>
2023-01-06 21:39:26 -0700

    V4

---
+++ scripts/bump-version.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Bumping version from ${1} to ${2}"
+echo "Replacing version in selfupdate.go"
+sed -i '' s/$1/$2/g ./internal/update.go
+echo "Replacing version in launcher.exe.manifest"
+sed -i '' s/$1\.0/$2\.0/g ./build/windows/launcher.exe.manifest
