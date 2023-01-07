00fcdabc35b1b7679cf0c9be3512faa17ab62835
Coleby Kent <coleby210@gmail.com>
2023-01-06 22:59:44 -0700

    v10

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -5,3 +5,5 @@ echo "Replacing version in selfupdate.go"
 sed -i '' s/$1/$2/g ./internal/update.go
 echo "Replacing version in launcher.exe.manifest"
 sed -i '' s/$1\.0/$2\.0/g ./build/windows/launcher.exe.manifest
+echo "Replacing version in package.json"
+sed -i '' s/$1/$2/g ./frontend/package.json
