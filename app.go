0610b905039b9dc6cb5a5e267722c74ccb146ce4
Coleby Kent <coleby210@gmail.com>
2023-01-06 21:04:17 -0700

    Initial commit

---
+++ app.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+)
+
+// App struct
+type App struct {
+	ctx context.Context
+}
+
+// NewApp creates a new App application struct
+func NewApp() *App {
+	return &App{}
+}
+
+// startup is called when the app starts. The context is saved
+// so we can call the runtime methods
+func (a *App) startup(ctx context.Context) {
+	a.ctx = ctx
+}
+
+// Greet returns a greeting for the given name
+func (a *App) Greet(name string) string {
+	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+// domReady is called after the front-end dom has been loaded
+func (a *App) domReady(ctx context.Context) {
+	runtime.LogInfo(b.ctx, "Checking For Updates")
+	a.UpdateCheckUI()
+}
+
+func (a *App) UpdateCheckUI() {
+	shouldUpdate, latestVersion := update.CheckForUpdate()
+	if shouldUpdate {
+		updateMessage := fmt.Sprintf("New Version Available, would you like to update to v%s", latestVersion)
+		buttons := []string{"Yes", "No"}
+		dialogOpts := runtime.MessageDialogOptions{Title: "Update Available", Message: updateMessage, Type: runtime.QuestionDialog, Buttons: buttons, DefaultButton: "Yes", CancelButton: "No"}
+		action, err := runtime.MessageDialog(b.ctx, dialogOpts)
+		if err != nil {
+			runtime.LogError(b.ctx, "Error in update dialog. ")
+		}
+		runtime.LogInfo(b.ctx, action)
+		if action == "Yes" {
+			runtime.LogInfo(b.ctx, "Update clicked")
+			var updated bool
+			if goruntime.GOOS == "darwin" {
+				updated = update.DoSelfUpdateMac()
+			} else {
+				updated = update.DoSelfUpdate()
+			}
+			if updated {
+				buttons = []string{"Ok"}
+				dialogOpts = runtime.MessageDialogOptions{Title: "Update Succeeded", Message: "Update Successfull. Please restart this app to take effect. ", Type: runtime.InfoDialog, Buttons: buttons, DefaultButton: "Ok"}
+				runtime.MessageDialog(b.ctx, dialogOpts)
+			} else {
+				buttons = []string{"Ok"}
+				dialogOpts = runtime.MessageDialogOptions{Title: "Update Error", Message: "Update failed, please manually update from GitHub Releases. ", Type: runtime.InfoDialog, Buttons: buttons, DefaultButton: "Ok"}
+				runtime.MessageDialog(b.ctx, dialogOpts)
+			}
+		}
+	}
+}
